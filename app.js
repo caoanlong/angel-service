51953a183fe38a3cc5391e2fe8a494198cfd3f07
caoanlong <476977540@qq.com>
2018-07-02 23:34:02 +0800

    first blood

---
+++ app.js
@@ -0,0 +1,28 @@
+const Koa = require('koa')
+const logger = require('koa-logger')
+const bodyParser = require('koa-bodyparser')
+const cors = require('koa2-cors')
+
+const router = require('./routers')
+
+const app = new Koa()
+
+app.use(bodyParser())
+app.use(logger())
+
+app.use(cors({
+    origin: function (ctx) {
+        if (ctx.url === '/test') {
+            return "*"; // 允许来自所有域名请求
+        }
+        return ctx.request.header.origin
+        // return 'http://localhost:8080' // 这样就能只允许 http://localhost:8080 这个域名的请求了
+    },
+    exposeHeaders: ['Accept', 'X-Access-Token'],
+    allowMethods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+    allowHeaders: ['Content-Type', 'Accept', 'X-Access-Token']
+}))
+
+app.use(router.routes())
+
+module.exports = app
---
+++ config/index.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mysql: {
+        host: '47.106.171.37',
+        port: 3306,
+        user: 'root',
+        password: 'a1989204',
+        database: 'fuli_db'
+    },
+    redis: {
+
+    },
+    jwtConfig: {
+        'secret': 'caoanLong_xXoo_Fuck_yoU',
+        'exp': { expiresIn: 60 * 60 * 24 * 365 }
+    }
+}
