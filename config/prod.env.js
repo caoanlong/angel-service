bbbf02254e5c26c128daf3286b39bee31e6bf6f3
Caoanlong <caoanlong@gmail.com>
2018-08-01 19:56:38 +0800

    ggg

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,11 +1,11 @@
 module.exports = {
 	port: 3002,
 	mysql: {
-        host: '47.106.171.37',
+        host: 'rm-wz9z0k2jfpf2ikzy3.mysql.rds.aliyuncs.com',
         port: 3306,
         user: 'root',
-        password: 'a1989204',
-        database: 'fuli_db'
+        password: 'aA12345678',
+        database: 'angel'
     },
 	redis: {
 		host: '192.168.1.48',
